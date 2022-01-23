bff2748f0fb6b7ca03681461fb1334b491999d91
Santiago Melo Medina <santiagomelomedina@Santiagos-Mac-mini.local>
2022-01-23 10:39:12 -0500

    added description for pipy

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from distutils.core import setup
 
+README_FILE = open("README.md", 'r').read()
+
 setup(
     name="singleton-injector",
     packages=["singleton_injector"],
-    version="0.1",
+    version="0.2",
     license="MIT",
     description="Simple library to unify singleton and injection with a decorator",
+    long_description=README_FILE,
+    long_description_content_type='text/markdown',
     author="Santiago Melo Medina",
     author_email="smelomedina05@gmail.com",
     url="https://github.com/santiagoMeloMedina/singleton-injector",
@@ -17,13 +21,10 @@ setup(
     ],
     install_requires=[],
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Build Tools",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.8",
     ],
 )
